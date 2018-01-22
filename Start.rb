07c36951881e69d683b923752bf4290a65dedc88
Your Name <you@example.com>
2018-01-23 01:43:21 +0300

    method def

--- Start.rb
+++ Start.rb
@@ -1,12 +1,17 @@
 
-variableA = ARGV[0].to_i
-a = 0
 
-while a < 10
-  if variableA > 2
-    puts "True"
-  elsif variableA <= 2
-    puts "False"
+def methodSimple
+  variableA = ARGV[0].to_i
+  a = 0
+
+  while a < 10
+    if variableA > 2
+      puts "True"
+    elsif variableA <= 2
+      puts "False"
+    end
+    a += 1
   end
-  a += 1
 end
+
+methodSimple
