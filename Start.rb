f88c58832ce3d5993bbf4cec285badccea5f31b3
Your Name <you@example.com>
2018-01-24 01:03:55 +0300

    Default function arguments

--- Start.rb
+++ Start.rb
@@ -1,10 +1,15 @@
 
 
-def methodSimple
-  variableA = ARGV[0].to_i
-  a = 0
+def methodSimple(variableA = 0 ,a = 0)
 
-  while a < 10
+  if variableA == 0 && a == 0      # if variable is empty
+    variableA = ARGV[0].to_i
+  else
+    variableA = 5
+    a = 0
+  end
+
+   while a < 10
     if variableA > 2
       puts "True"
     elsif variableA <= 2
